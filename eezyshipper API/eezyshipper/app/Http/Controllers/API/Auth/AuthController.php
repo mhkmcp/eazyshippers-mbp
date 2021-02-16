07de1fcda2d6e2b27b400ee18edfd3021b9246cc
Saqlain Mustak <56887504+SaqlainCSE@users.noreply.github.com>
2021-02-16 17:18:40 +0600

    Create AuthController.php

---
+++ eezyshipper API/eezyshipper/app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function auth_user()
+    {
+        $user = User::find(Auth::user()->id);
+        if (is_null($user)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'no auth user',
+            ], 200);
+        }
+        return response()->json([
+            'success' => true,
+            'message' => 'authenticated user details fetched',
+            'data' => [
+                'user' => $user,
+            ]
+        ], 200);
+    }
+}
